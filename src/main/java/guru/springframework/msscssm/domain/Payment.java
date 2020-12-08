bfcb0eed45c070e17bae1588fc7ef5dbf52b6515
Ignacio Garcia Gimenez <ignaciogarciagimenez@gmail.com>
2020-12-08 20:48:57 -0300

    full tutorial ready

---
+++ src/main/java/guru/springframework/msscssm/domain/Payment.java
@@ -0,0 +1,29 @@
+package guru.springframework.msscssm.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Payment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentState state;
+
+    private BigDecimal amount;
+
+}
